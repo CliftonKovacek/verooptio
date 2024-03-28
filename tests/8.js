c861d600398f08473fd732ec20d39ea206324fe6
CliftonKovacek <juliebenfordf69e3b0825173@outlook.com>
2024-03-29 07:27:35 +0800

    asperiores

---
+++ tests/8.js
@@ -0,0 +1,95 @@
+/* test #8
+ * testing how literalizer works
+ */
+
+return /a/g;
+throw /a/g;
+delete /a/g;
+in /a/g;
+else /a/g;
+void /a/g;
+typeof /a/g;
+yield /a/g;
+(/a/g)
+[/a/g]
+{/a/g}
++/a/g;
+-/a/g;
+*/a/g;
+=/a/g;
+~/a/g;
+!/a/g;
+%/a/g;
+&/a/g;
+,/a/g;
+|/a/g;
+;/a/g;
+:/a/g;
+?/a/g;
+</a/g;
+>/a/g;
+0/ /a/g;
+/a/ / /a/;
+/a/;
+/a/g;
+/a/,
+/a/);
+/a/%
+/a/ ;
+/a/;
+if(a)/a/g;
+while  (a||(b)&&(c||d)) /a/g;
+for (;;)  
+/a/g;
+(a)/a/g;
+(a) (a) /a/g;
+{a:/a/g};
+{a:
+/a/g};
+{}/a/g;
+{a:{}/a/g};
+{a:{}
+/a/g}
+a={a:/a/g};
+{a:{a:/a/g}};
+a={a:{a:/a/g}};
+{a();a:/a/g;a:a();a:/a/g;};
+{a:function(){}/a/g};
+{a:
+function(){}/a/g}
+{a:{a:function(){}/a/g}};
+{a:
+	{a:
+		function(){}/a/g}};
+a={a:function(){}/a/g};
+a={a:{a:function(){}/a/g}};
+a
+=
+{a:{a:function(){}/a/g}};
+a{a:{a:function(){}/a/g}};
+{a:{a={a:function(){a={a:a/a/g}/a/g}(/a/g)/a/g}/a/g}/a/g}/a/g;
+a
+// a comment
+{a:{a:function(){}/a/g}};
+a
+/* a comment */
+	{a:{a:function(){}/a/g}};
+function* foo(){/a/g}/a/g;
+function*(){/a/g}/a/g;
+a=function* foo(){/a/g}/a/g;
+a=function*(){/a/g}/a/g;
+a=>/a/g;
+a=>a/a/g;
+"a"
+{}/a/g;
+a/a/g;
+a
+/a/g;
+a
+{}/a/g;
+/ / / / /;
+/\/g\//g;
+/\//g;
+/\/\//g;
+/[/]/g;
+/a/
